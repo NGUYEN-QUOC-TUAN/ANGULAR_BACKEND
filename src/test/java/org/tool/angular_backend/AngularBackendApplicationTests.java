6e1152973be1ac944d2f21bda9a632801eb194d4
fakerphong <fakerphong@gmail.com>
2025-01-07 11:44:25 +0700

    07-01-2025

---
+++ src/test/java/org/tool/angular_backend/AngularBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tool.angular_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
